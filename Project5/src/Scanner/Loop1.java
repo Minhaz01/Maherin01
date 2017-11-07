7b9217d48a41df6818038018c6dd5963e42684e4
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-07 17:43:18 -0600

    Update Loop1.java
    hay

--- Project5/src/Scanner/Loop1.java
+++ Project5/src/Scanner/Loop1.java
@@ -4,7 +4,8 @@ public class Loop1 {
 
 	public static void main(String[] args) {
 	System.out.println(" Still trying gti");
-
+	System.out.println(" Still trying gti2");
+	System.out.println(" Still trying gti3");	
 	}
 
 }
